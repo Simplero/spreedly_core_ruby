c7989878d783a26e0e916fb46293414241c25b19
Calvin Conaway <calvin@conaway.com>
2012-11-12 15:56:25 +0530

    Fixed gateway redaction

--- lib/spreedly_core_ruby/gateway.rb
+++ lib/spreedly_core_ruby/gateway.rb
@@ -57,13 +57,8 @@ module SpreedlyCore
 
     # Redact sensitive attributes of a gateway
     def self.redact(token)
-      opts = {
-        :headers => {"Content-Type" => "application/xml"},
-        :body => gateway_options.to_xml(:root => :gateway, :dasherize => false)
-      }
-
-      self.class.verify_post("/gateways/#{token}/redact.xml", opts) do |response|
-        return new response.parsed_response["gateway"]
+      self.class.verify_put("/gateways/#{token}/redact.xml", body: {}, has_key: 'transaction') do |response|
+        return new response.parsed_response["transaction"]
       end
     end
 
