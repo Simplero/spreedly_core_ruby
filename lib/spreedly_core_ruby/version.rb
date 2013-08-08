d6c924dadc6c3a335f9bf57cf236baa1e14ed0b1
Calvin Conaway <calvin@conaway.com>
2013-08-08 16:47:53 +0200

    Typo - I'm getting tired

--- lib/spreedly_core_ruby/version.rb
+++ lib/spreedly_core_ruby/version.rb
@@ -1,4 +1,4 @@
 module SpreedlyCore
   Version = VERSION = "0.3.0" unless const_defined?(VERSION)
-  ApiVersion = API_VERSION = "v1" unless const_defined?((API_VERSION)
+  ApiVersion = API_VERSION = "v1" unless const_defined?(API_VERSION)
 end
