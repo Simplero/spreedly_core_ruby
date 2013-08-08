128a2eafa05ea7a2cd8b41ff6cdc73c409b55507
Calvin Conaway <calvin@conaway.com>
2013-08-08 16:47:03 +0200

    Let's fall back to this solution

--- lib/spreedly_core_ruby/version.rb
+++ lib/spreedly_core_ruby/version.rb
@@ -1,8 +1,4 @@
 module SpreedlyCore
-  puts "============================================="
-  puts "SpreedlyCore - defining VERSION"
-  puts caller
-  puts "============================================="
-  Version = VERSION = "0.3.0" #unless const_defined?(VERSION)
-  ApiVersion = API_VERSION = "v1" #unless const_defined?((API_VERSION)
+  Version = VERSION = "0.3.0" unless const_defined?(VERSION)
+  ApiVersion = API_VERSION = "v1" unless const_defined?((API_VERSION)
 end
