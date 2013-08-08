b26474b21e6414125fb302759da2aec731eb1c35
Calvin Conaway <calvin@conaway.com>
2013-08-08 16:18:31 +0200

    Even more obvious

--- lib/spreedly_core_ruby/version.rb
+++ lib/spreedly_core_ruby/version.rb
@@ -1,5 +1,8 @@
 module SpreedlyCore
+  puts "============================================="
+  puts "SpreedlyCore - defining VERSION"
   puts caller
+  puts "============================================="
   Version = VERSION = "0.3.0"
   ApiVersion = API_VERSION = "v1"
 end
